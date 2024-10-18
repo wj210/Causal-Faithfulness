efa09e73c10521b424a60414d11a7123325831e8
weijie210 <weijie210@scsehg.cm.cluster>
2024-10-18 11:02:21 +0800

    changed files

--- get_score.sh
+++ get_score.sh
@@ -1,15 +1,3 @@
-echo "Running job with CUDA_VISIBLE_DEVICES set to: $CUDA_VISIBLE_DEVICES"
-cuda_path="cuda_visible_devices.txt"
-if [ -f $cuda_path ]; then
-  export CUDA_VISIBLE_DEVICES=$(cat $cuda_path)
-  num_gpu=$(cat "$cuda_path" | tr ', ' '\n' | grep -c '[0-9]')
-  echo "num gpus = $num_gpu"
-else
-  echo "cuda_visible_devices.txt file not found."
-fi
-
-
-
 model_name='gemma2-9B'
 dataset_name='comve'
 
